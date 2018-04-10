d1ae341be6468aef3da1cfa1e4cb984747c7375c
Miguel Fondeur <miguelfondeur@gmail.com>
2018-04-10 12:51:52 -0400

    initial commit

---
+++ gulpfile.babel.js
@@ -0,0 +1,113 @@
+const gulp 				= require('gulp'),
+			postcss 		= require('gulp-postcss'),
+			babel 			= require('gulp-babel'),
+			imagemin		= require('gulp-imagemin'), 
+			del 				= require('del'),
+			browserify 	= require('browserify'),
+			htmlreplace = require('gulp-html-replace');
+
+// Plugins
+const cssnext 		= require('postcss-cssnext'),
+			css_import 	= require('postcss-import'), 
+			cssnano 		= require('cssnano'),
+			uglify			= require('gulp-uglify'),
+			cache 			= require('gulp-cache'),
+			runSequence = require('run-sequence'),
+			buffer 			= require('vinyl-buffer'),
+			source 			= require('vinyl-source-stream'),
+			babelify		= require('babelify'),
+			htmlmin			= require('gulp-htmlmin'),
+			rename 			= require("gulp-rename"),
+			urlAdjuster = require('gulp-css-url-adjuster');
+
+// HTML
+gulp.task('html', function() {
+	return gulp.src('./src/*.html')
+		.pipe(htmlreplace({
+				'css': 'styles.min.css',
+				'js': 'bundle.min.js'
+		}))
+		.pipe(htmlmin({collapseWhitespace: true}))
+		.pipe(gulp.dest('./dist'))
+});
+
+// CSS
+gulp.task('css', () => {
+	const config = (file) => ({
+		plugins: [
+			require('postcss-import')({ root: './src/css/main.css' })
+		]
+	});
+	var processors = [
+		cssnext({
+			browsers: 'last 2 versions',
+			warnForDuplicates: false
+		}),
+		cssnano
+	];
+	return gulp.src('./src/css/main.css')
+		.pipe(postcss(config))
+		.pipe( postcss(processors) )
+		.pipe(urlAdjuster({
+			replace:  ['../../',''],
+		}))
+		.pipe( rename('styles.min.css'))
+		.pipe( gulp.dest('./dist/') )
+});
+
+// Scripts
+gulp.task('scripts', function () {
+	var bundler = browserify('./src/js/app.js');
+	bundler.transform(babelify);
+
+	bundler.bundle()
+		.on('error', function (err) { console.error(err); })
+		.pipe(source('app.js'))
+		.pipe(buffer())
+		.pipe(uglify())
+		.pipe( rename('bundle.min.js'))
+		.pipe(gulp.dest('./dist'));
+});
+
+// Images
+gulp.task('images', function(){
+  return gulp.src('./src/img/**/*.+(png|jpg|jpeg|gif|svg)')
+  // Caching images that ran through imagemin
+		.pipe(cache(imagemin({
+				interlaced: true
+			})))
+		.pipe(gulp.dest('./dist/img'))
+});
+
+// Fonts
+gulp.task('fonts', function() {
+  return gulp.src('./src/fonts/**/*')
+  	.pipe(gulp.dest('./dist/fonts'))
+})
+
+// Clean Dist
+gulp.task('clean:dist', function() {
+  return del.sync('./dist');
+})
+
+// Build Watch
+// gulp.task('watch', ['browserSync', 'css'], function (){
+//   gulp.watch('./src/css//**/*.css', ['css']); 
+//   gulp.watch('app/*.html', browserSync.reload); 
+//   gulp.watch('app/js/**/*.js', browserSync.reload); 
+// });
+
+// Gulp Default
+gulp.task('default', function() {
+	gulp.watch('./src/js/*.js', ['scripts']);
+	gulp.watch('./src/css/**/*.css', ['css']);
+	gulp.watch('./src/*.html', ['html']);
+});
+
+// Build Task
+gulp.task('build', (callback) => {
+	runSequence('clean:dist', 
+		['html','css', 'scripts', 'images', 'fonts'],
+	callback
+	)	
+});
---
+++ src/js/app.js
@@ -0,0 +1,51 @@
+// import { scrollIt } from './scroll';
+
+/**
+ * Run event after DOM is ready
+ * @param  {Function} fn Callback function
+ */
+var ready = function ( fn ) {
+	// Sanity check
+	if ( typeof fn !== 'function' ) return;
+	// If document is already loaded, run method
+	if ( document.readyState === 'interactive' || document.readyState === 'complete' ) {
+			return fn();
+	}
+	// Otherwise, wait until document is loaded
+	document.addEventListener( 'DOMContentLoaded', fn, false );
+};
+
+//Event Delegation
+function scrollTo(e) {
+	e.preventDefault();
+  if (e.target.matches('.scroll')){ 
+		target = e.target.getAttribute("href");
+		document.querySelector(target).scrollIntoView({behavior: 'smooth',block: "start"});
+	}
+}
+
+function expandCard(e) {
+	document.querySelector('#work').scrollIntoView({behavior: 'smooth',block: "start"});
+	//use class to open fixed page overlay and position paged fixed
+	document.body.classList.toggle('with-overlay');
+	//remove active class from all other cards?
+	//add active class to target card and remove active class
+	e.target.classList.toggle('active');
+}
+
+// When ready
+ready(function() {
+
+	//Variables
+	const scrollLink = document.querySelector('.scroll');
+	const siteNav = document.querySelector('.site-nav');
+	const workCards = document.querySelector('.work-card-wrapper');	
+
+	//Event Delegation
+	siteNav.addEventListener('click', scrollTo);
+	workCards.addEventListener('click', expandCard);
+
+
+});
+
+
